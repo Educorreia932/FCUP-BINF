c13a9b756d1927502a3c9498f3e74cfa011a771f
Educorreia932 <skelozard1@gmail.com>
2022-03-22 14:43:56 +0000

    Exercise 3 from Class 3 done

--- Exercises/Basic Processing of Sequences/Exercise 2/sequence_functions.py
+++ Exercises/Basic Processing of Sequences/Exercise 2/sequence_functions.py
@@ -1,15 +1,17 @@
 from rich import print
 
+
 def read_seq_from_file(filename):
     """ Reads a sequence from a multi-line text file. """
 
     fh = open(filename, "r")
     lines = fh.readlines()
     seq = "".join(line.replace("\n", "") for line in lines)
-    
+
     fh.close()
-    
+
     return seq
+
 
 def write_seq_to_file(seq, filename):
     """ Writes a sequence to file. """
@@ -17,46 +19,54 @@ def write_seq_to_file(seq, filename):
     with open(filename, "w") as outfile:
         outfile.writelines([seq[i:i + 60] + "\n" for i in range(0, len(seq), 60)])
 
+
 def read_genetic_code_from_file(filename):
     """ Reads the genetic code to a dictionary from a multi-line text file. """
 
     with open(filename, "r") as f:
-        return {line[0]:line[1] for line in map(lambda x: x.replace("\"", "").split(), f.readlines())} 
+        return {line[0]: line[1] for line in map(lambda x: x.replace("\"", "").split(), f.readlines())}
 
-def validate_dna (dna_seq):
+
+def validate_dna(dna_seq):
     """ Checks if DNA sequence is valid. Returns True is sequence is valid, or False otherwise. """
 
     seqm = dna_seq.upper()
 
     return all(x in {"A", "C", "G", "T"} for x in seqm)
 
-def frequency (seq):
+
+def frequency(seq):
     """ Calculates the frequency of each symbol in the sequence. Returns a dictionary. """
 
-    return {x:seq.count(x) for x in set(seq)}
+    return {x: seq.count(x) for x in set(seq)}
 
-def gc_content (dna_seq):
+
+def gc_content(dna_seq):
     """ Returns the percentage of G and C nucleotides in a DNA sequence. """
     gc_count = 0
     for s in dna_seq:
-        if s in "GCgc": gc_count += 1
+        if s in "GCgc":
+            gc_count += 1
     return gc_count / len(dna_seq)
 
-def gc_content_subseq (dna_seq, k=100):
+
+def gc_content_subseq(dna_seq, k=100):
     """ Returns GC content of non-overlapping sub-sequences of size k. """
     # complete
     # ...
 
-def transcription (dna_seq):
+
+def transcription(dna_seq):
     """ Function that computes the RNA corresponding to the transcription of the DNA sequence provided. """
     assert validate_dna(dna_seq), "Invalid DNA sequence"
 
     return dna_seq.upper().replace("T", "U")
 
-def reverse_complement (dna_seq):
+
+def reverse_complement(dna_seq):
     """ Computes the reverse complement of the inputted DNA sequence. """
     assert validate_dna(dna_seq), "Invalid DNA sequence"
- 
+
     return dna_seq[::-1] \
         .replace("A", "t") \
         .replace("C", "g") \
@@ -64,39 +74,41 @@ def reverse_complement (dna_seq):
         .replace("T", "A") \
         .upper()
 
-def translate_codon (cod):
+
+def translate_codon(cod):
     """Translates a codon into an aminoacid using an internal dictionary with the standard genetic code."""
 
     tc = {
-        "GCT":"A", "GCC":"A", "GCA":"A", "GCG":"A",
-        "TGT":"C", "TGC":"C",
-        "GAT":"D", "GAC":"D",
-        "GAA":"E", "GAG":"E",
-        "TTT":"F", "TTC":"F",
-        "GGT":"G", "GGC":"G", "GGA":"G", "GGG":"G",
-        "CAT":"H", "CAC":"H",
-        "ATA":"I", "ATT":"I", "ATC":"I",
-        "AAA":"K", "AAG":"K",
-        "TTA":"L", "TTG":"L", "CTT":"L", "CTC":"L", "CTA":"L", "CTG":"L",
-        "ATG":"M", "AAT":"N", "AAC":"N",
-        "CCT":"P", "CCC":"P", "CCA":"P", "CCG":"P",
-        "CAA":"Q", "CAG":"Q",
-        "CGT":"R", "CGC":"R", "CGA":"R", "CGG":"R", "AGA":"R", "AGG":"R",
-        "TCT":"S", "TCC":"S", "TCA":"S", "TCG":"S", "AGT":"S", "AGC":"S",
-        "ACT":"T", "ACC":"T", "ACA":"T", "ACG":"T",
-        "GTT":"V", "GTC":"V", "GTA":"V", "GTG":"V",
-        "TGG":"W",
-        "TAT":"Y", "TAC":"Y",
-        "TAA":"_", "TAG":"_", "TGA":"_"
+        "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+        "TGT": "C", "TGC": "C",
+        "GAT": "D", "GAC": "D",
+        "GAA": "E", "GAG": "E",
+        "TTT": "F", "TTC": "F",
+        "GGT": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+        "CAT": "H", "CAC": "H",
+        "ATA": "I", "ATT": "I", "ATC": "I",
+        "AAA": "K", "AAG": "K",
+        "TTA": "L", "TTG": "L", "CTT": "L", "CTC": "L", "CTA": "L", "CTG": "L",
+        "ATG": "M", "AAT": "N", "AAC": "N",
+        "CCT": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+        "CAA": "Q", "CAG": "Q",
+        "CGT": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+        "TCT": "S", "TCC": "S", "TCA": "S", "TCG": "S", "AGT": "S", "AGC": "S",
+        "ACT": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+        "GTT": "V", "GTC": "V", "GTA": "V", "GTG": "V",
+        "TGG": "W",
+        "TAT": "Y", "TAC": "Y",
+        "TAA": "_", "TAG": "_", "TGA": "_"
     }
-    
-    if cod in tc: 
+
+    if cod in tc:
         return tc[cod]
-    
-    else: 
+
+    else:
         return None
 
-def translate_seq (dna_seq, ini_pos = 0):
+
+def translate_seq(dna_seq, ini_pos=0):
     """Translates a DNA sequence into an aminoacid sequence."""
 
     assert validate_dna(dna_seq), "Invalid DNA sequence"
@@ -105,18 +117,20 @@ def translate_seq (dna_seq, ini_pos = 0):
 
     return "".join(translate_codon(seqm[i:i + 3]) for i in range(ini_pos, len(seqm) - 2, 3))
 
+
 def codon_usage(dna_seq, aa):
     """Provides the frequency of each codon encoding a given aminoacid, in a DNA sequence ."""
 
     assert validate_dna(dna_seq), "Invalid DNA sequence"
-    
+
     seqm = dna_seq.upper()
 
     aa_codons = [seqm[i:i + 3] for i in range(0, len(seqm) - 2, 3) if codon == aa]
-    
-    return {codon:aa_codons.count() / len(aa_codons) for codon in set(aa_codons)}
 
-def reading_frames (dna_seq):
+    return {codon: aa_codons.count() / len(aa_codons) for codon in set(aa_codons)}
+
+
+def reading_frames(dna_seq):
     """Computes the six reading frames of a DNA sequence (including the reverse complement."""
 
     assert validate_dna(dna_seq), "Invalid DNA sequence"
@@ -127,11 +141,12 @@ def reading_frames (dna_seq):
         res.append(translate_seq(dna_seq, i))
 
     rc = reverse_complement(dna_seq)
-    
+
     for i in range(3):
         res.append(translate_seq(rc, i))
-    
+
     return res
+
 
 def all_proteins_rf(aa_seq):
     """Computes all posible proteins in an aminoacid sequence."""
@@ -145,15 +160,16 @@ def all_proteins_rf(aa_seq):
                 proteins.append(p)
 
             current_prot = []
-        
+
         else:
             if aa == "M":
                 current_prot.append("")
-            
+
             for i in range(len(current_prot)):
                 current_prot[i] += aa
 
     return proteins
+
 
 def all_orfs(dna_seq):
     """Computes all possible proteins for all open reading frames."""
@@ -164,27 +180,31 @@ def all_orfs(dna_seq):
 
     return res
 
-def all_orfs_ord(dna_seq, minsize = 0):
+
+def all_orfs_ord(dna_seq, minsize=0):
     """Computes all possible proteins for all open reading frames. Returns ordered list of proteins with minimum size."""
-    
+
     assert validate_dna(dna_seq), "Invalid DNA sequence"
-    
+
     return list(filter(lambda x: len(x) > minsize, sorted(all_orfs(dna_seq), key=lambda x: len(x), reverse=True)))
 
-def insert_prot_ord (prot, list_prots):
+
+def insert_prot_ord(prot, list_prots):
     """inserts prot in list_prots in a sorted way"""
 
     i = 0
 
     while i < len(list_prots) and len(prot) < len(list_prots[i]):
         i += 1
-    
+
     list_prots.insert(i, prot)
+
 
 def longest_protein(dna_seq):
     orfs = all_orfs_ord(dna_seq)
 
     return orfs[0]
+
 
 def possible_proteins(dna_seq):
     proteins = {}
@@ -199,16 +219,18 @@ def possible_proteins(dna_seq):
 
     return proteins
 
+
 def test_frequency():
     seq_aa = input("Protein sequence:")
     freq_aa = frequency(seq_aa)
-    list_f = sorted(freq_aa.items(), key=lambda x: x[1], reverse = True)
+    list_f = sorted(freq_aa.items(), key=lambda x: x[1], reverse=True)
 
-    for (k,v) in list_f:
+    for (k, v) in list_f:
         print("Aminoacid:", k, ":", v)
 
+
 def test_all(seq):
-    if validate_dna (seq):
+    if validate_dna(seq):
         assert validate_dna(seq), "Invalid DNA sequence"
 
         print("Valid sequence\n")
@@ -224,20 +246,43 @@ def test_all(seq):
 
         print(f"[bold]Longest protein:[/bold] {longest_protein(seq)}\n")
         print(f"[bold]Possible proteins:[/bold] {possible_proteins(seq)}")
-    
-    else: 
+
+    else:
         print("DNA sequence is not valid")
+
+
+def read_fasta_2dictionary(filename):
+    result = {}
+    sequence = ""
+
+    with open(filename) as fasta:
+        for line in fasta.readlines():
+            if line[0] == ">":
+                identifier = line.split()[0][1:]
+
+                if sequence != "":
+                    result[identifier] = sequence
+
+                sequence = ""
+
+            else:
+                sequence += line.strip()
+
+    return result
 
 def test_files():
     fname = input("Insert input filename:")
     test_sequence_from_file(fname)
+
 
 def test_sequence_from_file(filename):
     seq = read_seq_from_file(filename)
 
     test_all(seq)
 
+
 if __name__ == "__main__":
     # write_seq_to_file("ATGAGCGACAT" * 10, "seq.txt")
     # test_sequence_from_file("example_Hinfluenzae.txt")
-    test_sequence_from_file("genomic_dna.fa")
+    # test_sequence_from_file("genomic_dna.fa")
+    print(read_fasta_2dictionary("PS00727.fasta"))
