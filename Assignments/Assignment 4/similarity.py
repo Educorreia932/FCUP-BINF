5efab16c23a40ad3176cbcf27f638460f942c6fd
Educorreia932 <skelozard1@gmail.com>
2022-05-01 11:19:00 +0100

    Assignment 4 - Changes in exercises 1 and 3

--- Assignments/Assignment 4/similarity.py
+++ Assignments/Assignment 4/similarity.py
@@ -1,3 +1,9 @@
+"""
+Bruno Vaz - up201705247
+Eduardo Correia - up201806433
+Filipe Justiça - up201606339 
+"""
+
 from bioseq.BioSequence import BioSequence
 from bioseq.Alignment import NeedlemanWunsch, SmithWaterman
 
@@ -17,44 +23,68 @@ def count_mismatches(s1, s2):
 
 # 1)
 
-needleman_wunsch = NeedlemanWunsch(seq1, seq2, -8)
-needleman_wunsch.calculate()
-s1, s2 = needleman_wunsch.recover_align()
+score_matrix = []
+
+for i, p1 in enumerate(list(proteins.items())):
+    score_matrix.append([])
+
+    # Add elements that were already calculated
+    # We only calculate them once, since similarity between A and B is the same as B and A
+    for j in range(0, i):
+        score_matrix[-1].append(score_matrix[j][i])
+
+    for p2 in list(proteins.items())[i:]:
+        needleman_wunsch = NeedlemanWunsch(p1[1], p2[1], -8)
+        needleman_wunsch.calculate()
+
+        score_matrix[-1].append((p2[0], needleman_wunsch.alignment_score, needleman_wunsch.recover_align()))
+
+scores_list = list(sorted(score_matrix[0], key=lambda item: item[1], reverse=True))
 
 print(f"Exercise 1:")
-print(f"\tThe number of mismatches in global alignment is {count_mismatches(s1, s2)}.")
-print()
+print("\t<Protein>: <Score>")
+
+for score in scores_list:
+    print(f"\t{score[0]}: {score[1]}")
 
 # 2)
 
-smith_waterman = SmithWaterman(seq1, seq2, -8)
-smith_waterman.calculate()
-s1, s2 = smith_waterman.recover_align()
+# 3)
 
-print(f"Exercise 2:")
-print(f"\tThe number of mismatches in local alignment is {count_mismatches(s1, s2)}.")
+mismatch_matrix = []
+
+for i, _ in enumerate(score_matrix):
+    mismatch_matrix.append([])
+
+    for p2 in score_matrix[i]:
+        mismatch_matrix[-1].append(count_mismatches(*p2[2]))
+
+def plot_matrix(matrix):
+    print("{}".format("".join('\t' + str(i) for i in range(len(matrix[0])))))
+
+    for i, p1 in enumerate(matrix):
+        print(f"{i}\t", end="")
+
+        for j, p2 in enumerate(matrix[i]):
+            print(f"{matrix[i][j]}\t", end="")
+
+        print()
+
+print()
+print("Exercise 3:")
+
+print()
+print("Scores matrix:")
 print()
 
-# 3)
-ref_protein = seq1
-protas_score = {}
-for prota in list(proteins.keys())[1:]:
-    seq = proteins[prota]
+plot_matrix([[p[1] for p in row] for row in score_matrix])
 
-    needleman_wunsch = NeedlemanWunsch(ref_protein, seq, -8)
-    needleman_wunsch.calculate()
-    protas_score[prota] = needleman_wunsch.alignment_score
+print()
+print("Mismatches matrix:")
+print()
 
-scores_list = list(
-    sorted(protas_score.items(), key=lambda item: item[1], reverse=True)
-)
-
-print(f"Exercise 3:")
-print("\t Protein: Score")
-for tuplo in scores_list:
-    print(f"\t {tuplo[0]}: {tuplo[1]}")
-
-
+plot_matrix(mismatch_matrix)
 
 # 4)
 
+# 5)
